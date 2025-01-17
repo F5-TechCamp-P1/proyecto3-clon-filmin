ebe3ee392395af88e8b51a53db75a54c493d2f8c
MabelRincon <mabel.rincon@outlook.com>
2025-01-17 23:23:35 +0100

    Change logo, adjust fonts

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,9 +8,9 @@ const NavBar = () => {
             {/* Logo */}
             <a href="index.html" className="flex items-center">
                 <img
-                src="../src/assets/Logo_Filmin_2023.png"
+                src="../src/assets/logo.svg"
                 alt="Filmin Logo"
-                className="h-6 ml-12 mt-1.5"
+                className="h-6 ml-11 mt-1.5 mb-1.5"
                 />
             </a>
 
@@ -18,22 +18,22 @@ const NavBar = () => {
             <div className="relative">
               <button
                 onClick={() => setMenuOpen(!menuOpen)}
-                className="text-white focus:outline-none"
+                className="text-white focus:outline-none active:text-teal-400 font-bold mr-4"
               >
                 Más
               </button>
 
               {menuOpen && (
-                <div className="absolute right-0 mt-2 bg-white text-black rounded shadow-lg">
+                <div className="absolute right-0 w-max mt-2 bg-gray-900 text-white font-bold rounded shadow-lg">
                     <ul>
-                        <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                            Opción 1111
+                        <li className="px-12 py-4 hover:bg-gray-800 cursor-pointer">
+                            El mejor cine reciente en exclusiva
                         </li>
-                        <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                            Opción 2
+                        <li className="px-12 py-4 hover:bg-gray-800 cursor-pointer">
+                            Series imprescindibles para todos los gustos
                         </li>
-                        <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                            Opción 3
+                        <li className="px-12 py-4 hover:bg-gray-800 cursor-pointer">
+                            Las candidatas al Óscar internacional de 2024
                         </li>
                     </ul>
                </div>
