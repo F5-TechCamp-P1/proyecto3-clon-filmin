1e30d4bfeac54f59c5736cf5a3f0b698989cbc60
unknown <gaviriayeferson2@gmail.com>
2025-01-21 15:35:10 +0100

    Fix bug component Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,34 @@
-import React, {useState, useContext} from 'react'
-import CardWithDetails from '../components/CardWithDetails'
-import { ListTypeContext } from '../App'
+import React, { useState, useContext } from 'react';
+import CardWithDetails from '../components/CardWithDetails';
+import { ListTypeContext } from '../App';
 
-function Card ({imgUrl}){
-    let listType = useContext(ListTypeContext)
+function Card ({ imgUrl, title, description, genres, rating }) {
+    let listType = useContext(ListTypeContext);
     
-    const [hover, setHover] = useState(false)
+    const [hover, setHover] = useState(false);
     
-
-    function handleHover (){
-        setHover(true)
+    function handleHover() {
+        setHover(true);
     }
 
-    function handleHoverOut (){
-        setHover(false)
+    function handleHoverOut() {
+        setHover(false);
     }
 
-    return(
+    return (
         <>
-        <div> 
-            <img src={imgUrl} onMouseEnter={handleHover} onMouseLeave={handleHoverOut} alt={title}/>
-        </div>
-        {hover && listType === "seriesList" && ( 
             <div> 
-             <CardWithDetails title={title} description={description} genres={genres} rating={rating} />} /> 
-            </div> )}
+                <img src={imgUrl} onMouseEnter={handleHover} onMouseLeave={handleHoverOut} alt={title} />
+            </div>
+            {hover && listType === "seriesList" && ( 
+                <div> 
+                    <CardWithDetails title={title} description={description} genres={genres} rating={rating} /> 
+                </div> 
+            )}
         </>
-    )
+    );
 }
-export default Card
+
+export default Card;
+
+
