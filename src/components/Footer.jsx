c3f5c27986db357acaf0fd9685991197acca64a9
Angelica-2025 <angelica-2025@outlook.es>
2025-01-22 13:56:27 +0100

    change done footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,15 @@ function Footer () {
     return (
         <>
         
-        <footer class="bg-black rounded-lg shadow m-4 dark:bg-gray-800">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="http://localhost:5173/" class="hover:underline">YRAMMP™</a>. All Rights Reserved.
+        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
             </span>
-                <ul class="flex flex-wrap items-center mt- text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                    <li><a href="#" class="hover:underline me-4 md:me-6">Mabel</a></li>
-                    <li><a href="#" class="hover:underline me-4 md:me-6">Ayelen</a></li>
-                    <li><a href="#" class="hover:underline me-4 md:me-6">Yeferson</a></li>
-                    <li><a href="#" class="hover:underline me-4 md:me-6">Patricia</a></li>
-                    <li><a href="#" class="hover:underline me-4 md:me-6">Rosa</a></li>
-
+                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li><a href="#" className="hover:underline me-4 md:me-6">About</a></li>
+                    <li><a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a></li>
+                    <li><a href="#" className="hover:underline me-4 md:me-6">Licensing</a></li>
+                    <li><a href="#" className="hover:underline">Contact</a></li>
                 </ul>
             </div>
         </footer>
