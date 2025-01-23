d30ae56a94cb1746be9a59f3679383d00c46cfd4
Ayuik <marcosayelen@gmail.com>
2025-01-23 08:13:19 +0100

    several styles added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,15 +7,19 @@ function List({ listTitle, fetchList }) {
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchList();
-      setItems(data.slice(0, 5));
+      setItems(data.slice(0, 15));
     };
     fetchData();
   }, [fetchList]);
 
   return (
-    <div>
-      <h2>{listTitle}</h2>
-      <CardsSection items={items} />
+    <div className="flex flex-col justify-center items-center w-full">
+      <div className="justify-self-start self-start">
+        <h1 className="font-bold Graphik Medium text-white ml-3 ">{listTitle}</h1>
+      </div>
+      <div className="w-full p-7">
+        <CardsSection items={items} />
+      </div>     
     </div>
   );
 }
