e16f3cf22ab30c9a9724ccab17d1afcabdd23b30
Angelica-2025 <angelica-2025@outlook.es>
2025-01-23 09:02:18 +0100

    add mates links git

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,16 @@ function Footer () {
     return (
         <>
         
-        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <footer className="bg-black px-4 py-3 flex items-center justify-between">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+            <span className="text-sm text-white font-bold rounded shadow-lg">© 2025 <a href="http://localhost:5173/" className="hover:underline">YRAMMP™</a>. All Rights Reserved.
             </span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                    <li><a href="#" className="hover:underline me-4 md:me-6">About</a></li>
-                    <li><a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a></li>
-                    <li><a href="#" className="hover:underline me-4 md:me-6">Licensing</a></li>
-                    <li><a href="#" className="hover:underline">Contact</a></li>
+                <ul className="flex flex-wrap items-center mt-3 font-medium text-white font-bold rounded shadow-lg">
+                    <li><a href="#" className="hover:underline me-4 md:me-6">Patricia</a></li>
+                    <li><a href="https://github.com/YefGav" className="hover:underline me-4 md:me-6">Yeferson</a></li>
+                    <li><a href="https://github.com/MabelRincon" className="hover:underline me-4 md:me-6">Mabel</a></li>
+                    <li><a href="https://github.com/Ayuik" className="hover:underline me-4 md:me-6">Ayelen</a></li>
+                    <li><a href="https://github.com/Angelica-2025" className="hover:underline me-4 md:me-6">Rosa</a></li>
                 </ul>
             </div>
         </footer>
@@ -25,3 +26,4 @@ function Footer () {
 }
 
 export default Footer
+
