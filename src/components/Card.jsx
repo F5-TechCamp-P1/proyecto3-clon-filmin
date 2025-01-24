68bfab6fe4ef73a18e2528150decf8d66ec7a8d6
Yeferson Gaviria <122923711+YefGav@users.noreply.github.com>
2025-01-24 11:16:11 +0100

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,28 +2,38 @@ import React, { useState, useContext } from 'react';
 import CardWithDetails from '../components/CardWithDetails';
 import { ListTypeContext } from '../App';
 
-function Card({ imgUrl, title, description, genres, rating }) {
-    const listType = useContext(ListTypeContext);
-    const [hover, setHover] = useState(false);
+function Card ({ imgUrl, title, description, genres, rating }) {
+  const listType = useContext(ListTypeContext);
+  const [hover, setHover] = useState(false);
 
-    return (
+  function handleHover() {
+    setHover(true);
+  }
+
+  function handleHoverOut() {
+    setHover(false);
+  }
+
+  return (
+    <div 
+      className="m-2 relative"
+      onMouseEnter={handleHover} 
+      onMouseLeave={handleHoverOut}
+    >
+      <img 
+        src={imgUrl} 
+        alt={title} 
+        className="rounded hover:border hover:border-gray-500"
+      />
+      {hover && listType === "seriesList" && (
         <div 
-            className="m-2 relative"
-            onMouseEnter={() => setHover(true)} 
-            onMouseLeave={() => setHover(false)}
-        >
-            <img 
-                src={imgUrl} 
-                alt={title} 
-                className="rounded hover:border hover:border-gray-500"
-            />
-            <div 
-                className={`z-10 absolute inset-0 flex items-center justify-center transition-opacity duration-300 ${hover && listType === "seriesList" ? 'opacity-100' : 'opacity-0'}`}
-            > 
-                <CardWithDetails title={title} description={description} genres={genres} rating={rating} /> 
-            </div> 
+          className={`z-10 absolute inset-0 flex items-center justify-center transition-opacity duration-300 ${hover ? 'opacity-100' : 'opacity-0'}`}
+        > 
+          <CardWithDetails title={title} description={description} genres={genres} rating={rating} /> 
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Card;
